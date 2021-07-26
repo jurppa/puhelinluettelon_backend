3c7192289dc202b96ce25eda60708ac8c06e5572
jurppa <78047234+jurppa@users.noreply.github.com>
2021-07-26 14:50:57 +0300

    3.15 puhelinluettelo ja tietokanta

--- index.js
+++ index.js
@@ -66,15 +66,24 @@ app.get("/api/persons/:id", (req, res) => {
   }
 });
 // Delete person
-app.delete("/api/persons/:id", (req, res) => {
+/* app.delete("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
   filteredPersons = taulukko.filter((person) => person.id !== id);
 
   res.status(204).end();
+}); */
+app.delete("/api/persons/:id", (req, response, next) => {
+  console.log("Poistettavan id: " + req.params.id);
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => console.log(error));
 });
 // Post person
 
 app.post("/api/persons", (req, res) => {
+  console.log("post request");
   const body = req.body;
 
   if (body.name === undefined) {

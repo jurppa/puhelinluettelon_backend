bf90ce98290394bec8eb081d8f9b9757983fe8f7
jurppa <78047234+jurppa@users.noreply.github.com>
2021-07-26 09:00:42 +0300

    3.12 tietokanta komentoriviltä

---
+++ mongo.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const password = process.argv[2];
+const url = `mongodb+srv://fullstack:${password}@cluster0.0es8j.mongodb.net/phonebookdb?retryWrites=true&w=majority`;
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+const phonebookSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+});
+const Person = mongoose.model("Person", phonebookSchema);
+// Save person to mongodb
+if (process.argv.length === 5) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
+
+  person.save().then((response) => {
+    console.log("person saved!");
+    mongoose.connection.close();
+  });
+}
+console.log(process.argv.length);
+if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person);
+    });
+    mongoose.connection.close();
+  });
+}
